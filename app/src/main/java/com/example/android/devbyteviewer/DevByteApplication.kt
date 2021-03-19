f99ad64d77a385db9403901f697fc09be3437ad8
Eric <ericsilva328@gmail.com>
2021-03-19 19:26:12 -0300

    work manager apllied

--- app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -1,7 +1,15 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.example.android.devbyteviewer.work.RefreshDataWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 /**
  * Override application to setup background work via WorkManager
@@ -14,8 +22,29 @@ class DevByteApplication : Application() {
      * Use it to setup any background tasks, running expensive setup operations in a background
      * thread to avoid delaying app start.
      */
+
+    private val aplicationScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        delayInit()
+    }
+
+    private fun delayInit(){
+        aplicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
+    }
+
+    private fun setupRecurringWork(){
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(5, TimeUnit.MINUTES).build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+                RefreshDataWorker.WORK_NAME,
+                ExistingPeriodicWorkPolicy.KEEP,
+                repeatingRequest
+        )
     }
 }
